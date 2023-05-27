6bf7659c0743564bf7c8a4d511dff33ba20a20ee
Alexander Barnard <alexwb1@live.com>
2023-05-27 14:44:27 +0700

    Updated the default phrases

--- src/Models/Default.ts
+++ src/Models/Default.ts
@@ -1,18 +1,6 @@
 
 const defaultPhraseList = [
-    {"english_text":"The apple is red","translation":"แอปเปิ้ลเป็นสีแดง"},
-    {"english_text":"It is John's apple","translation":"เป็นแอปเปิ้ลของจอห์น"},
-    {"english_text":"I give John the apple","translation":"ฉันให้จอห์นแอปเปิ้ล"},
-    {"english_text":"We give him the apple","translation":"เราให้แอปเปิ้ลแก่เขา"},
-    {"english_text":"He gives it to John","translation":"เขามอบให้กับจอห์น"},
-    {"english_text":"She gives it to him","translation":"เธอมอบให้เขา"},
-    {"english_text":"Is the apple red?","translation":"แอปเปิ้ลแดงหรือไม่?"},
-    {"english_text":"The apples are red","translation":"แอปเปิ้ลเป็นสีแดง"},
-    {"english_text":"I must give it to him","translation":"ฉันต้องมอบให้เขา"},
-    {"english_text":"I want to give it to her","translation":"ฉันต้องการมอบให้เธอ"},
-    {"english_text":"I'm going to know tomorrow","translation":"ฉันจะรู้พรุ่งนี้"},
-    {"english_text":"I can't eat the apple","translation":"ฉันกินแอปเปิ้ลไม่ได้"},
-    {"english_text":"I have eaten the apple","translation":"ฉันกินแอปเปิ้ล"}
-  ];
+  {"english_text":"The mango is yellow","translation":"มะม่วงเป็นสีเหลือง"},{"english_text":"It is John's mango","translation":"มันคือมะม่วงของจอห์น"},{"english_text":"I give John the mango","translation":"ฉันให้จอห์นมะม่วง"},{"english_text":"We give him the mango","translation":"เราให้มะม่วงแก่เขา"},{"english_text":"He gives it to John","translation":"เขามอบให้กับจอห์น"},{"english_text":"She gives it to him","translation":"เธอมอบให้เขา"},{"english_text":"Is the mango yellow?","translation":"มะม่วงเป็นสีเหลืองหรือไม่?"},{"english_text":"The mangos are yellow","translation":"มังโกสเป็นสีเหลือง"},{"english_text":"I must give it to him","translation":"ฉันต้องมอบให้เขา"},{"english_text":"I want to give it to her","translation":"ฉันต้องการมอบให้เธอ"},{"english_text":"I'm going to know tomorrow","translation":"ฉันจะรู้พรุ่งนี้"},{"english_text":"I can't eat the mango","translation":"ฉันกินมะม่วงไม่ได้"},{"english_text":"I have eaten the mango","translation":"ฉันกินมะม่วง"}
+];
 
 export default defaultPhraseList;
