18c32ff3b8269d7d9c131d80a0191d87c8516bfa
Alexander Barnard <alexwb1@live.com>
2023-06-07 16:24:06 +0700

    Added first 3 presets

--- src/Assets/PresetList.ts
+++ src/Assets/PresetList.ts
@@ -3,16 +3,36 @@ import Preset from "../Models/Preset";
 const presetList: Preset[] = [
   {
     "index": 1,
-    "topic": "Clothes",
-    "chapter": 5,
-    "page": 114,
-    "list": [{"english_text":"shirt","translation":"เสื้อ"},{"english_text":"socks","translation":"ถุงเท้า"},{"english_text":"hat","translation":"หมวก"},{"english_text":"pants","translation":"กางเกง"},{"english_text":"shoes","translation":"รองเท้า"},{"english_text":"jacket","translation":"เสื้อแจ็กเกต"},{"english_text":"panties","translation":"กางเกง"},{"english_text":"underwear","translation":"ชุดชั้นใน"},{"english_text":"glasses","translation":"แว่นตา"},{"english_text":"mask","translation":"หน้ากาก"},{"english_text":"uniform","translation":"เครื่องแบบ"},{"english_text":"sandals","translation":"รองเท้าแตะ"},{"english_text":"bra","translation":"บรา"}]
+    "topic": "Greeting",
+    "chapter": 2,
+    "page": 16,
+    "list": [{"english_text":"Hello","translation":"สวัสดี"},{"english_text":"Me (male)","translation":"ผม"},{"english_text":"Me (female)","translation":"ฉัน"},{"english_text":"you (formal)","translation":"คุณ"},{"english_text":"She","translation":"เธอ"},{"english_text":"We/us","translation":"เรา"},{"english_text":"He/she/him/her","translation":"เขา"},{"english_text":"They/them","translation":"พวกเขา"},{"english_text":"Name","translation":"ชื่อ"},{"english_text":"Surname","translation":"นามสกุล"},{"english_text":"Excuse me","translation":"ขอโทษ"},{"english_text":"Know","translation":"รุ้จัก"},{"english_text":"Nice to meet you","translation":"ยินดีที่ได้รู้จัก"},{"english_text":"too","translation":"เช่นกัน"},{"english_text":"to feel fine","translation":"สบายดี"},{"english_text":"to not feel fine","translation":"ไม่สบาย"},{"english_text":"what","translation":"อะไร"},{"english_text":"yes/no question","translation":"ไหม/มั้ย"},{"english_text":"And you?","translation":"(แล้ว)...ล่ะ"},{"english_text":"yes (female)","translation":"คุ่ะ"},{"english_text":"final particle (female)","translation":"คะ"},{"english_text":"final particle (male)","translation":"ครับ"}]
   },
   {
     "index": 2,
+    "topic": "Where are you from?",
+    "chapter": 2,
+    "page": 24,
+    "list": [{"english_text":"Country","translation":"ประเทศ"},{"english_text":"state","translation":"รัฐ"},{"english_text":"city","translation":"เมือง"},{"english_text":"district","translation":"อำเภอ"},{"english_text":"province","translation":"จังหวัด"},{"english_text":"Thailand","translation":"ไทย"},{"english_text":"Japan","translation":"ประเทศญี่ปุ่น"},{"english_text":"America","translation":"อเมริกา"},{"english_text":"British","translation":"ชาวอังกฤษ"},{"english_text":"Tokyo","translation":"โตเกียว"},{"english_text":"to come","translation":"มา"},{"english_text":"from","translation":"จาก"},{"english_text":"to come from","translation":"มาจาก"},{"english_text":"where","translation":"ที่ไหน"},{"english_text":"to be","translation":"เป็น"},{"english_text":"to be at","translation":"อยุ่"},{"english_text":"to live at","translation":"พัก"},{"english_text":"nationality","translation":"ชาว"},{"english_text":"of","translation":"ของ"},{"english_text":"at","translation":"ที่"},{"english_text":"near","translation":"ใกล้"},{"english_text":"area","translation":"แถว"},{"english_text":"house","translation":"บ้าน"},{"english_text":"hotel","translation":"โรงแรม"},{"english_text":"condominium","translation":"คอนโดมิเนียม"},{"english_text":"friend","translation":"เพื่อน"},{"english_text":"student","translation":"นักเรียน"},{"english_text":"teacher","translation":"ครู"},{"english_text":"Chiang Mai University","translation":"มหาวิทยาลัยเชียงใหม่"},{"english_text":"MAYA","translation":"เมย่า"},{"english_text":"Central Festival","translation":"เซ็นทรัล เฟสติวัล"}],
+  },
+  {
+    "index": 3,
+    "topic": "What is this? Who is that?",
+    "chapter": 3,
+    "page": 38,
+    "list": [{"english_text":"this","translation":"นี้"},{"english_text":"that","translation":"นั่น"},{"english_text":"over there","translation":"โน่น"},{"english_text":"is","translation":"คือ"},{"english_text":"call","translation":"เรียก"},{"english_text":"what","translation":"อะไร"},{"english_text":"who","translation":"ใคร"},{"english_text":"whose","translation":"ของใคร"},{"english_text":"of + name","translation":"ของ + ชื่อ"},{"english_text":"yes","translation":"ใช่"},{"english_text":"no","translation":"ไม่ใช่"},{"english_text":"Question word for yes/no ","translation":"ใช่ไหม"},{"english_text":"pen","translation":"ปากกา"},{"english_text":"pencil","translation":"ดินสอ"},{"english_text":"book","translation":"หนังสือ"},{"english_text":"table","translation":"โต๊ะ"},{"english_text":"chair","translation":"เก้าอี้"},{"english_text":"bag (backpack)","translation":"กระเป๋า"},{"english_text":"bag","translation":"ถุง"},{"english_text":"mobile phone","translation":"มือถือ"},{"english_text":"friend","translation":"เพื่อน"},{"english_text":"to be","translation":"เป็น"}],
+  },
+  // all presets above are directly from book
+  {
+    "topic": "Clothes",
+    "chapter": 5,
+    "page": 115,
+    "list": [{"english_text":"shirt","translation":"เสื้อ"},{"english_text":"socks","translation":"ถุงเท้า"},{"english_text":"hat","translation":"หมวก"},{"english_text":"pants","translation":"กางเกง"},{"english_text":"shoes","translation":"รองเท้า"},{"english_text":"jacket","translation":"เสื้อแจ็กเกต"},{"english_text":"panties","translation":"กางเกง"},{"english_text":"underwear","translation":"ชุดชั้นใน"},{"english_text":"glasses","translation":"แว่นตา"},{"english_text":"mask","translation":"หน้ากาก"},{"english_text":"uniform","translation":"เครื่องแบบ"},{"english_text":"sandals","translation":"รองเท้าแตะ"},{"english_text":"bra","translation":"บรา"}]
+  },
+  {
     "topic": "Colors",
     "chapter": 4,
-    "page": 105,
+    "page": 122,
     "list": [{"english_text":"black","translation":"สีดำ"},{"english_text":"grey","translation":"สีเทา"},{"english_text":"white","translation":"สีขาว"},{"english_text":"pink","translation":"สีชมพู"},{"english_text":"red","translation":"สีแดง"},{"english_text":"orange","translation":"ส้ม"},{"english_text":"yellow","translation":"สีเหลือง"},{"english_text":"green","translation":"สีเขียว"},{"english_text":"light blue","translation":"สีฟ้า"},{"english_text":"dark blue","translation":"น้ำเงิน"},{"english_text":"purple","translation":"สีม่วง"},{"english_text":"brown","translation":"สีน้ำตาล"},{"english_text":"silver","translation":"เงิน"},{"english_text":"gold","translation":"ทอง"}]
   },
 ];
