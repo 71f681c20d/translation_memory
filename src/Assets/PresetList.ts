18c182ba057b38ea31e5d2d6387b19be01576895
Alexander Barnard <alexwb1@live.com>
2023-06-20 11:17:39 +0700

    Fixed titles of presets

--- src/Assets/PresetList.ts
+++ src/Assets/PresetList.ts
@@ -3,56 +3,56 @@ import Preset from "../Models/Preset";
 const presetList: Preset[] = [
   {
     "index": 1,
-    "topic": "Greeting",
+    "topic": "คุณชื่ออะไร (What is your name?)",
     "chapter": 2,
     "page": 16,
     "list": [{"english_text":"Hello","translation":"สวัสดี"},{"english_text":"Me (male)","translation":"ผม"},{"english_text":"Me (female)","translation":"ฉัน"},{"english_text":"you (formal)","translation":"คุณ"},{"english_text":"She","translation":"เธอ"},{"english_text":"We/us","translation":"เรา"},{"english_text":"He/she/him/her","translation":"เขา"},{"english_text":"They/them","translation":"พวกเขา"},{"english_text":"Name","translation":"ชื่อ"},{"english_text":"Surname","translation":"นามสกุล"},{"english_text":"Excuse me","translation":"ขอโทษ"},{"english_text":"Know","translation":"รุ้จัก"},{"english_text":"Nice to meet you","translation":"ยินดีที่ได้รู้จัก"},{"english_text":"too","translation":"เช่นกัน"},{"english_text":"to feel fine","translation":"สบายดี"},{"english_text":"to not feel fine","translation":"ไม่สบาย"},{"english_text":"what","translation":"อะไร"},{"english_text":"yes/no question","translation":"ไหม/มั้ย"},{"english_text":"And you?","translation":"(แล้ว)...ล่ะ"},{"english_text":"yes (female)","translation":"คุ่ะ"},{"english_text":"final particle (female)","translation":"คะ"},{"english_text":"final particle (male)","translation":"ครับ"}]
   },
   {
     "index": 2,
-    "topic": "Where are you from?",
+    "topic": "คุณมาจากที่ไหน (Where are you from?)",
     "chapter": 2,
     "page": 24,
     "list": [{"english_text":"Country","translation":"ประเทศ"},{"english_text":"state","translation":"รัฐ"},{"english_text":"city","translation":"เมือง"},{"english_text":"district","translation":"อำเภอ"},{"english_text":"province","translation":"จังหวัด"},{"english_text":"Thailand","translation":"ไทย"},{"english_text":"Japan","translation":"ประเทศญี่ปุ่น"},{"english_text":"America","translation":"อเมริกา"},{"english_text":"British","translation":"ชาวอังกฤษ"},{"english_text":"Tokyo","translation":"โตเกียว"},{"english_text":"to come","translation":"มา"},{"english_text":"from","translation":"จาก"},{"english_text":"to come from","translation":"มาจาก"},{"english_text":"where","translation":"ที่ไหน"},{"english_text":"to be","translation":"เป็น"},{"english_text":"to be at","translation":"อยุ่"},{"english_text":"to live at","translation":"พัก"},{"english_text":"nationality","translation":"ชาว"},{"english_text":"of","translation":"ของ"},{"english_text":"at","translation":"ที่"},{"english_text":"near","translation":"ใกล้"},{"english_text":"area","translation":"แถว"},{"english_text":"house","translation":"บ้าน"},{"english_text":"hotel","translation":"โรงแรม"},{"english_text":"condominium","translation":"คอนโดมิเนียม"},{"english_text":"friend","translation":"เพื่อน"},{"english_text":"student","translation":"นักเรียน"},{"english_text":"teacher","translation":"ครู"},{"english_text":"Chiang Mai University","translation":"มหาวิทยาลัยเชียงใหม่"},{"english_text":"MAYA","translation":"เมย่า"},{"english_text":"Central Festival","translation":"เซ็นทรัล เฟสติวัล"}],
   },
   {
     "index": 3,
-    "topic": "What is this? Who is that?",
+    "topic": "นี่คืออะไร นั่นคืใคร (What is this? Who is that?)",
     "chapter": 3,
     "page": 38,
     "list": [{"english_text":"this","translation":"นี้"},{"english_text":"that","translation":"นั่น"},{"english_text":"over there","translation":"โน่น"},{"english_text":"is","translation":"คือ"},{"english_text":"call","translation":"เรียก"},{"english_text":"what","translation":"อะไร"},{"english_text":"who","translation":"ใคร"},{"english_text":"whose","translation":"ของใคร"},{"english_text":"of + name","translation":"ของ + ชื่อ"},{"english_text":"yes","translation":"ใช่"},{"english_text":"no","translation":"ไม่ใช่"},{"english_text":"Question word for yes/no ","translation":"ใช่ไหม"},{"english_text":"pen","translation":"ปากกา"},{"english_text":"pencil","translation":"ดินสอ"},{"english_text":"book","translation":"หนังสือ"},{"english_text":"table","translation":"โต๊ะ"},{"english_text":"chair","translation":"เก้าอี้"},{"english_text":"bag (backpack)","translation":"กระเป๋า"},{"english_text":"bag","translation":"ถุง"},{"english_text":"mobile phone","translation":"มือถือ"},{"english_text":"friend","translation":"เพื่อน"},{"english_text":"to be","translation":"เป็น"}],
   },
   {
     "index": 4,
-    "topic": "Fruit",
+    "topic": "ผลไม้ (Fruit)",
     "chapter": 6,
     "page": 98,
     "list": [{"english_text":"papaya","translation":"มะละกอ"},{"english_text":"pineapple","translation":"สัปปะรด"},{"english_text":"banana","translation":"กล้วย"},{"english_text":"mango","translation":"มะม่วง"},{"english_text":"longan","translation":"ลำไย"},{"english_text":"guava","translation":"ฝรั่ง"},{"english_text":"mangosteen","translation":"มังคุด"},{"english_text":"rambutan","translation":"เงาะ"},{"english_text":"durian","translation":"ทุเรียน"},{"english_text":"cantaloupe","translation":"แคนตาลูป"},{"english_text":"grape","translation":"องุ่น"},{"english_text":"lychee","translation":"ลิ้นจี่"},{"english_text":"apple","translation":"แอปเปิล"},{"english_text":"pomelo","translation":"ส้มโอ"},{"english_text":"orange","translation":"ส้ม"},{"english_text":"custard apple","translation":"น้อยหน่า"},{"english_text":"rose apple","translation":"ชมพู่"}]
   },
   {
     "index": 5,
-    "topic": "Bill, please",
+    "topic": "เก็บเงิน (Bill, please)",
     "chapter": 6,
     "page": 99,
     "list": [{"english_text":"money","translation":"เงิน"},{"english_text":"bill","translation":"ใบแจ้งหนี้"},{"english_text":"change","translation":"เงินทอน"},{"english_text":"all together","translation":"ทั้งหมด"},{"english_text":"how many","translation":"กี่"}]
   },
   {
     "index": 6,
-    "topic": "Going shopping, clothes, sizes",
+    "topic": ":ซื้อของ เสื้อผ้า และขนาด (Going shopping, clothes, sizes)",
     "chapter": 7,
     "page": 115,
     "list": [{"english_text":"shirt","translation":"เสื้อ"},{"english_text":"long-sleeved shirt","translation":"เสื้อแขนยาว"},{"english_text":"short-sleeved shirt","translation":"เสื้อแขนสั้น"},{"english_text":"coat","translation":"เสื้กันหนาว"},{"english_text":"pants","translation":"กางเกง"},{"english_text":"trousers","translation":"กางเกงขายาว"},{"english_text":"shorts","translation":"กางเกงขาสั้น"},{"english_text":"skirt","translation":"กระโปรง"},{"english_text":"socks","translation":"ถุงเท้า"},{"english_text":"shoes","translation":"รองเท้า"},{"english_text":"hat","translation":"หมวก"},{"english_text":"glasses","translation":"แว่นตา"},{"english_text":"belt","translation":"เข็มขัด"},{"english_text":"size","translation":"ขนาด"},{"english_text":"big","translation":"ใหญ่"},{"english_text":"small","translation":"เล็ก"},{"english_text":"too...","translation":"ไป"},{"english_text":"fit","translation":"พอดี"},{"english_text":"sold out","translation":"มอด"},{"english_text":"to try on","translation":"ลอง"}]
   },
   {
     "index": 7,
-    "topic": "Colors, things, payments",
+    "topic": "สี ของใช้ การจ่ายเงิน (Colors, things, payments)",
     "chapter": 7,
     "page": 122,
     "list": [{"english_text":"color","translation":"สี"},{"english_text":"black","translation":"สีดำ"},{"english_text":"white","translation":"สีขาว"},{"english_text":"grey","translation":"สีเทา"},{"english_text":"purple","translation":"สีม่วง"},{"english_text":"dark blue","translation":"น้ำเงิน"},{"english_text":"light blue","translation":"สีฟ้า"},{"english_text":"green","translation":"สีเขียว"},{"english_text":"yellow","translation":"สีเหลือง"},{"english_text":"orange","translation":"ส้ม"},{"english_text":"red","translation":"สีแดง"},{"english_text":"pink","translation":"สีชมพู"},{"english_text":"brown","translation":"สีน้ำตาล"},{"english_text":"gold","translation":"ทอง"},{"english_text":"silver","translation":"เงิน"},{"english_text":"dark","translation":".... + เข้ม"},{"english_text":"light","translation":".... + อ่อน"},{"english_text":"shampoo","translation":"แชมพู"},{"english_text":"hair conditioner","translation":"ครีมนวดผม"},{"english_text":"soap","translation":"สบู่"},{"english_text":"shower gel","translation":"สบู่เหลว"},{"english_text":"bar of soap","translation":"สบู่ก้อน"},{"english_text":"toothpaste","translation":"ยาสีฟัน"},{"english_text":"toothbrush","translation":"แปรงสีฟัน"},{"english_text":"dish soap","translation":"น้ำยาล้างจาน"},{"english_text":"detergent","translation":"ผงซักฟอก"},{"english_text":"cotton wool","translation":"สำลี"},{"english_text":"umbrella","translation":"ร่ม"},{"english_text":"raincoat","translation":"เสื้อกันฝน"},{"english_text":"piece","translation":"ก้อน"},{"english_text":"box","translation":"กล่อง"},{"english_text":"cash","translation":"เงินสด"},{"english_text":"credit card","translation":"บัตรเครดิต"},{"english_text":"bank transfer","translation":"โอนเงิน"},{"english_text":"to scan","translation":"สแกน"},{"english_text":"QR code","translation":"คิวอาร์โค้ด"},{"english_text":"together","translation":"ด้วยกัน"},{"english_text":"only","translation":"แค่"}]
   },
   {
     "index": 8,
-    "topic": "Day, month, time",
+    "topic": "วัน เดือน ปี และ เวลา (Day, month, time)",
     "chapter": 8,
     "page": 133,
     "list": [{"english_text":"day","translation":"วัน"},{"english_text":"Monday","translation":"วันจันทร์"},{"english_text":"Tuesday","translation":"วันอังคาร"},{"english_text":"Wednesday","translation":"วันพุธ"},{"english_text":"Thursday","translation":"วันพฤหัสบดี"},{"english_text":"Friday","translation":"วันศุกร์"},{"english_text":"Saturday","translation":"วันเสาร์"},{"english_text":"Sunday","translation":"วันอาทิตย์"},{"english_text":"week","translation":"สัปดาห์"},{"english_text":"weekend","translation":"สุดสัปดาห์"},{"english_text":"today","translation":"วันนี้"},{"english_text":"tomorrow","translation":"พรุ่งนี้"},{"english_text":"the day after tomorrow","translation":"วันมะรืนนี้"},{"english_text":"yesterday","translation":"เมื่อวาน"},{"english_text":"the day before yesterday","translation":"เมื่อวานซืน"},{"english_text":"when","translation":"เมื่อไร"},{"english_text":"next (week)","translation":"(สัปดาห์) หน้า"},{"english_text":"last (week)","translation":"(สัปดาห์) ที่แล้ว"}]
